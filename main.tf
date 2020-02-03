cc7b80d3aeba78ffb9ae512547a659819bb4e998
BrunoApSouza <brunoap500@gmail.com.br>
2020-02-03 20:03:26 -0300

    trabalho final

--- main.tf
+++ main.tf
@@ -17,18 +17,39 @@ resource "google_compute_instance" "helloworld" {
     access_config {
     }
   }
-}
 
-# Cria SQL Gerenciado com Engine MySQL 
-resource "google_sql_database_instance" "helloworldBruno" {
-  name             = "database"
-  database_version = "MYSQL_5_7"
-  region           = var.region
+  #Conexão SSH para validar Conectividade
+   provisioner "remote-exec" {
+     inline = ["echo 'Liberado para o Ansible'"]
 
-  settings {
-    tier = "db-f1-micro"
+     connection {
+       type        = "ssh"
+       user        = "bruno.souza.clc3"
+       private_key = file("../id_rsa")
+       host = google_compute_instance.helloworld.network_interface.0.access_config.0.nat_ip
+       timeout     = "90s"
+    }
+  } 
+
+  #Cria o inventário do Ansible
+  provisioner "local-exec" { 
+     command = "echo ${google_compute_instance.helloworld.network_interface.0.access_config.0.nat_ip} > ansible/inventory" 
   }
+
+  #Executa a playbook na máquina provisionada
+  provisioner "local-exec" {
+    command = "ansible-playbook -i ansible/inventory --private-key ../id_rsa ansible/playbook.yml"
+  }
+
 }
 
+#Cria SQL Gerenciado com Engine MySQL 
+#resource "google_sql_database_instance" "helloworldBruno" {
+#name             = "database"
+#database_version = "MYSQL_5_7"
+#region           = var.region
 
-
+#settings {
+#tier = "db-f1-micro"
+#  }
+#}
