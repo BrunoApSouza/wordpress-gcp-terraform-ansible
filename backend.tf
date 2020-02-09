eee85bfdaea644ab3a02aacfab1a0a973adfdc4d
BrunoApSouza <brunoap500@gmail.com.br>
2020-02-09 03:53:34 -0300

    atualizando trabalho 3

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform{
   backend "gcs" { 
    bucket = "tokyo-concept-260822-tfstate"
-   credentials = "../tokyo-concept-260822-359a7b6b1109.json"
+   credentials = "tokyo-concept-260822-359a7b6b1109.json"
    
  }
 }
--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
-  credentials = file("/vagrant/tokyo-concept-260822-359a7b6b1109.json")
+  credentials = file("vagrant/tokyo-concept-260822-359a7b6b1109.json")
   project     = var.project_id
   region      = var.region
 }
