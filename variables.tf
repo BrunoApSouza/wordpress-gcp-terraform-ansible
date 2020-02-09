92da17aa61f4789e2bd2fa7407f5c02fbf52e452
BrunoApSouza <brunoap500@gmail.com.br>
2020-02-09 01:19:02 -0300

    atualizando - trabalho final

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "project_id" {
+  description = "id do projeto"
+  default     = "tokyo-concept-260822"
+}
+
+variable "region" {
+  description = "descrição"
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "zona"
+  default     = "us-central1-a"
+}
+
+variable "mysql" {
+  description = "Descrição"
+  default     = "MYSQL_5_7"
+}
